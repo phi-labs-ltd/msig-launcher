682e0fd569dcf216dc2d8cace0b6520f65806a8d
Guy Garcia <guy.garcia@protonmail.com>
2024-12-17 12:02:02 -0400

    deployments

--- contracts/msig-launcher/src/bin/schema.rs
+++ contracts/msig-launcher/src/bin/schema.rs
@@ -1,11 +1,12 @@
 use cosmwasm_schema::write_api;
 
-use msig_launcher::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use msig_launcher::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
+        migrate: MigrateMsg
     }
 }
